8641e9c957c2166e484bba9d1339ad6897fc45f2
Luan Zaniqueli <luanzaniqueli.carlos@gmail.com>
2024-02-27 14:07:57 -0300

    feat(Data): [WEB-10] Created static data to be added to the database, if empty #done

--- Program.cs
+++ Program.cs
@@ -16,6 +16,13 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {

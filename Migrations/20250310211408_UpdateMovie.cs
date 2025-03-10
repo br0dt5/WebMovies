88b9baca27dfa170b58ca60df35bb4b873256d9f
Luan Zaniqueli <luanzaniqueli.carlos@gmail.com>
2025-03-10 18:17:45 -0300

    feat(movie): [WEB-37] Added extra metadata to the model #done

---
+++ Migrations/20250310211408_UpdateMovie.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebMovies.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateMovie : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Duration",
+                table: "Movies",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Keywords",
+                table: "Movies",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Duration",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "Keywords",
+                table: "Movies");
+        }
+    }
+}
